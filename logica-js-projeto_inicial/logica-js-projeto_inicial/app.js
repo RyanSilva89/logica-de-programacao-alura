b91996755400fd38947e73b02354182e0561350b
Ryan <r.sfariaa89@gmail.com>
2025-04-12 09:20:27 +0900

    observações sobre o funcionanmento:mensagem acertou mesmo errando,adicionado condional se/if

--- logica-js-projeto_inicial/logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/logica-js-projeto_inicial/app.js
@@ -3,4 +3,11 @@
 
 alert("Seja bem vindo ao Jogo número secreto");
 let numeroSecreto = 10;
-prompt("Chute um número entre 1 e 10");
+let chute=prompt("Chute um número entre 1 e 10");
+//ainda não está funcionando como o esperado,mesmo digitando o número secreto errado,ainda a tela de fundo está com a mensagem que a pessoa ganhou o jogo.
+console.log(numeroSecreto)
+//Condicionais
+if(numeroSecreto == chute){
+    alert ("Você acertou !");//se não for verdadeiro não irá mostrar a mesagem,será executando quando esssa condição for verdadeira.
+console.log("Você acertou!");//utilizado para fazer testes
+}
