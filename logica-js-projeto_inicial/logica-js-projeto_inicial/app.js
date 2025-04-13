fd6de6b21390d27a6a0627213a56efdf494e2741
Ryan <r.sfariaa89@gmail.com>
2025-04-13 15:45:00 +0900

    fix: correção da validação de idade no Desafio 1, garantindo que a idade seja numérica e não vazia antes da comparação.

--- logica-js-projeto_inicial/logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/logica-js-projeto_inicial/app.js
@@ -20,26 +20,22 @@ function iniciarDesafio1(){
              console.log(formatarNome(nomeDoUsuario));
              //Pedir idade 
 let idadeDoUsuario =prompt ("Digite a sua idade ")
-//Agora, caso a idade seja maior ou igual que 18, exiba um alerta com a mensagem "Pode tirar a habilitação!".
-if(idadeDoUsuario >= 18){
+//Garantindo que seja um nome válido
+if(isNaN(idadeDoUsuario || idadeDoUsuario.trim() === "" )){
+    alert("Por favor digite um número válido");
+    return;
+}
+idadeDoUsuario =  Number(idadeDoUsuario);
+//verifica se a idade é igual a 18 e se a idade é um número
+if(idadeDoUsuario >= 18 ){
     alert("Pode tirar a habilitação!");
+}else{
+    alert("Você não pode tirar a habilitação!");
+    alert("Volte quando vc tiver 18 anos ")
 }
     }
 
-    //variaveis Ficticias-Serão usadas na segunda versão
-        //Declare uma variável chamada nome e atribua a ela o valor "Lua".
-let nome = "Lua"
-//Crie uma variável chamada idade e atribua a ela o valor 25.
-
-    let idade = 25
-//Defina uma variável numeroDeVendas e atribua a ela o valor 50.
-let numeroDevendas = 50
-//Defina uma variável saldoDisponivel e atribua a ela o valor 1000.
-let saldoDisponivel = 1000
-//Exiba um alerta com o texto "Erro! Preencha todos os campos"
-//alert("Erro! Preencha todos os campos")
-//Declare uma variável chamada mensagemDeErro e atribua a ela o valor "Erro! Preencha todos os campos" Agora exiba um alerta com o valor da variável mensagemDeErro.
-let mensagemDeErro = "Erro!Preencha todos os campos"
+   
 // const botaoIniciar = document.getElementById('start-button');
 
 
