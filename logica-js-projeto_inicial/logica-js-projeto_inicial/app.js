64b6090cd03870e5a6e3273a19397d5f1607591b
Ryan <r.sfariaa89@gmail.com>
2025-04-13 14:04:35 +0900

    bug: Adicionei novamente um verificação inifinita

--- logica-js-projeto_inicial/logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/logica-js-projeto_inicial/app.js
@@ -15,10 +15,13 @@ let numeroDevendas = 50
 //Defina uma variável saldoDisponivel e atribua a ela o valor 1000.
 let saldoDisponivel = 1000
 //Exiba um alerta com o texto "Erro! Preencha todos os campos"
-alert("Erro! Preencha todos os campos")
+//alert("Erro! Preencha todos os campos")
 //Declare uma variável chamada mensagemDeErro e atribua a ela o valor "Erro! Preencha todos os campos" Agora exiba um alerta com o valor da variável mensagemDeErro.
 let mensagemDeErro = "Erro!Preencha todos os campos"
-
+while(mensagemDeErro == null){
+    alert("Você está encerrando a avaliação");
+    break;
+}
 //Use um prompt para perguntar o nome do usuário e armazená-lo na variável nome.
 let nomeDoUsuario = prompt("Digite o seu nome");
 
