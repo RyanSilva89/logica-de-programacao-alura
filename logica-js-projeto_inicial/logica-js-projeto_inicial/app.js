d99d36ce9a58ad1410ac1c61c434f05e6cec25b0
Ryan <r.sfariaa89@gmail.com>
2025-04-13 12:51:23 +0900

    revert: remove funções temporárias para foco na lógica principal

--- logica-js-projeto_inicial/logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/logica-js-projeto_inicial/app.js
@@ -21,21 +21,8 @@ let mensagemDeErro = "Erro!Preencha todos os campos"
 
 //Use um prompt para perguntar o nome do usuário e armazená-lo na variável nome.
 let nomeDoUsuario = prompt("Digite o seu nome");
-while(!nomeDoUsuario){
-    if(nomeDoUsuario == null){
-        break;
-    }
-    alert("Por favor,digite o seu nome,não deixe o espço em branco");
-    nomeDoUsuario = prompt("Digite o seu nome")
-    if(nomeDoUsuario){
-        nomeDoUsuario = formatarNome(nomeDoUsuario);
-        alert("olá" + nomeDoUsuario);
-    }
-}
-while(mensagemDeErro == null){
-    alert(mensagemDeErro);
-mensagemDeErro = null;
-}
+
+
 //Peça ao usuário para digitar sua idade usando um prompt e armazene-a na variável idade.
 let idadeDoUsuario =prompt ("Digite a sua idade ")
 //Agora, caso a idade seja maior ou igual que 18, exiba um alerta com a mensagem "Pode tirar a habilitação!".
