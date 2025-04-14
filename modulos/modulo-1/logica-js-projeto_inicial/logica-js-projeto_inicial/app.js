232825d2a534101c479a9e2fe0c7af6fb30e8472
Ryan <r.sfariaa89@gmail.com>
2025-04-14 20:56:03 +0900

    refactor: reorganizar a posição do console.log() sem alterar a lógica.

--- modulos/modulo-1/logica-js-projeto_inicial/logica-js-projeto_inicial/app.js
+++ modulos/modulo-1/logica-js-projeto_inicial/logica-js-projeto_inicial/app.js
@@ -3,9 +3,10 @@
 
 alert("Seja bem vindo ao Jogo número secreto");
 let numeroSecreto = 10;
+console.log(numeroSecreto)
 let chute=prompt("Chute um número entre 1 e 10");
 //ainda não está funcionando como o esperado,mesmo digitando o número secreto errado,ainda a tela de fundo está com a mensagem que a pessoa ganhou o jogo.
-console.log(numeroSecreto)
+
 //Condicionais
 if(numeroSecreto == chute){
     alert ("Você acertou !");//se não for verdadeiro não irá mostrar a mesagem,será executando quando esssa condição for verdadeira.
