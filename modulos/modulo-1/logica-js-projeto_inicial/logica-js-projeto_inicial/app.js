47ae4f9f9d680039b5cdb85bcf417e70c02e70f1
Ryan <r.sfariaa89@gmail.com>
2025-04-14 22:05:22 +0900

    docs: adiciona orientações para os desafios no código.

---
+++ modulos/modulo-1/logica-js-projeto_inicial/logica-js-projeto_inicial/app.js
@@ -0,0 +1,16 @@
+// 1. Pergunte ao usuário qual é o dia da semana.
+//    - Se a resposta for "Sábado" ou "Domingo", mostre "Bom fim de semana!".
+//    - Caso contrário, mostre "Boa semana!".
+
+// 2. Verifique se um número digitado pelo usuário é positivo ou negativo.
+//    - Mostre um alerta informando se o número é positivo ou negativo.
+
+// 3. Crie um sistema de pontuação para um jogo.
+//    - Se a pontuação for maior ou igual a 100, mostre "Parabéns, você venceu!".
+//    - Caso contrário, mostre "Tente novamente para ganhar.".
+
+// 4. Crie uma mensagem que informa o usuário sobre o saldo da conta.
+//    - Use uma template string para incluir o valor do saldo na mensagem.
+
+// 5. Peça ao usuário para inserir seu nome usando o prompt.
+//    - Em seguida, mostre um alerta de boas-vindas usando o nome inserido pelo usuário.
