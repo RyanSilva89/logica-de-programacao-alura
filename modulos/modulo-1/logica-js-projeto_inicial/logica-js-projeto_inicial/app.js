3b8cee2e78e67abf5024a706292342bb4a778c17
Ryan <r.sfariaa89@gmail.com>
2025-04-15 18:48:33 +0900

    feat: adiciona lógica para verificar dia da semana e número positivo/negativo.

--- modulos/modulo-1/logica-js-projeto_inicial/logica-js-projeto_inicial/app.js
+++ modulos/modulo-1/logica-js-projeto_inicial/logica-js-projeto_inicial/app.js
@@ -1,8 +1,20 @@
 // 1. Pergunte ao usuário qual é o dia da semana.
+let diaSemana = prompt("Qual é o dia da semana?");
 //    - Se a resposta for "Sábado" ou "Domingo", mostre "Bom fim de semana!".
+if(diaSemana === "Sábado"  ||  diaSemana === "Domingo"){
+    alert("Bom final de semana!");
+}else{
+    alert("Boa semana");
+}
 //    - Caso contrário, mostre "Boa semana!".
 
 // 2. Verifique se um número digitado pelo usuário é positivo ou negativo.
+if(num >= 0){
+    alert("O número é positivo");
+}else if (num< 0){
+    alert("O número é negativo");
+
+}
 //    - Mostre um alerta informando se o número é positivo ou negativo.
 
 // 3. Crie um sistema de pontuação para um jogo.
