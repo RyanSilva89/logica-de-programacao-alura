e8c36fa8792d5b2a3b2c4f5978cf39917d967706
Ryan <r.sfariaa89@gmail.com>
2025-04-19 06:43:03 +0900

    refactor: usa operador ternário para lidar com singular/plural nas tentativas

--- modulos/modulo-1/logica-js-projeto_inicial/logica-js-projeto_inicial/app.js
+++ modulos/modulo-1/logica-js-projeto_inicial/logica-js-projeto_inicial/app.js
@@ -11,24 +11,26 @@ let tentativas = 1;
 //while -enquanto
 //diferente !=
 while(chute != numeroSecreto){
-        chute = prompt("Chute um número entre 1 e 10 ");
-//Condicionais
-            if(numeroSecreto == chute){
-                break;
-        console.log("Você acertou!");//utilizado para fazer testes
-    
-            } else{
-            alert("Você errou!");//se não for verdadeiro,irá mostra a mensgem da condição se não/else.
-            }if(chute > numeroSecreto){
-            alert(`O número secreto é menor que o chute ${chute}e vc tem ${tentativas } tentativas até o momento`);
-        }else{
-            alert (`o número secreto é maior que o chute  ${chute} e vc tem ${tentativas } tentativas até o momento`);
+            chute = prompt("Chute um número entre 1 e 10 ");
+    //Condicionais
+                if(numeroSecreto == chute){
+                    // para sair do loop
+                    break;
+                
+                } else{
+                alert("Você errou!");//se não for verdadeiro,irá mostra a mensgem da condição se não/else.
+                }if(chute > numeroSecreto){
+                alert(`O número secreto é menor que o chute ${chute}`);
+            }else{
+                alert (`o número secreto é maior que o chute  ${chute}`);
+        }
+        tentativas++;
     }
-    tentativas++;
-    }
-if(tentativas == 1){
-    alert(`O número secreto era ${numeroSecreto} e você acertou com apenas ${tentativas} tentativa`);
-}else{
-    alert(`O número secreto era ${numeroSecreto} e você acertou com apenas ${tentativas} tentativas`);
-}
- alert (`Você acertou ! O número secreto é ${numeroSecreto} com ${tentativas} tentativas`);//se não for verdadeiro não irá mostrar a mesagem,será executando quando esssa condição for verdadeira.
+    let palavraTentativa = tentativas == 1 ? ' tentativa': ' tentativas';
+    alert(`O número secreto era ${numeroSecreto} e você acertou com apenas ${tentativas} ${palavraTentativa}`);
+
+   // if(tentativas == 1){
+   //     alert(`O número secreto era ${numeroSecreto} e você acertou com apenas uma ${tentativas} tentativa`);
+   // }else{
+   //     alert(`O número secreto era ${numeroSecreto} e você acertou com apenas ${tentativas} tentativas`);
+   // }
