462834148b264a2e0b05e4b87397996098899479
Ryan <r.sfariaa89@gmail.com>
2025-04-18 20:06:39 +0900

    Feat: adicionado laço de repetição while colocado a logiga/condiconais dentro do while Função break para parar o codigo ao acertar,saindo do laço.

--- modulos/modulo-1/logica-js-projeto_inicial/logica-js-projeto_inicial/app.js
+++ modulos/modulo-1/logica-js-projeto_inicial/logica-js-projeto_inicial/app.js
@@ -4,17 +4,23 @@
 alert("Seja bem vindo ao Jogo número secreto");
 let numeroSecreto = 6;
 console.log(numeroSecreto)
-let chute=prompt("Chute um número entre 1 e 10");
+let chute
 //ainda não está funcionando como o esperado,mesmo digitando o número secreto errado,ainda a tela de fundo está com a mensagem que a pessoa ganhou o jogo.
-
+//enquanto o número secreto for diferente do chute,irá mostrar a mensagem de que a pessoa errou.
+//while -enquanto
+//diferente !=
+while(chute != numeroSecreto){
+        chute = prompt("Chute um número entre 1 e 10 ");
 //Condicionais
-    if(numeroSecreto == chute){
-    alert (`Você acertou ! O número secreto é ${numeroSecreto}`);//se não for verdadeiro não irá mostrar a mesagem,será executando quando esssa condição for verdadeira.
-console.log("Você acertou!");//utilizado para fazer testes
+        if(numeroSecreto == chute){
+            alert (`Você acertou ! O número secreto é ${numeroSecreto}`);//se não for verdadeiro não irá mostrar a mesagem,será executando quando esssa condição for verdadeira.
+    console.log("Você acertou!");//utilizado para fazer testes
+    break;
+        }else{
+        alert("Você errou!");//se não for verdadeiro,irá mostra a mensgem da condição se não/else.
+        }if(chute > numeroSecreto){
+        alert(`O número secreto é menor que o chute ${chute}`);
     }else{
-    alert("Você errou!");//se não for verdadeiro,irá mostra a mensgem da condição se não/else.
-}if(chute > numeroSecreto){
-    alert(`O número secreto é menor que o chute${chute}`);
-}else{
-    alert (`o número secreto é maior que o chute${chute}`);
+        alert (`o número secreto é maior que o chute  ${chute}`);
+}
 }
