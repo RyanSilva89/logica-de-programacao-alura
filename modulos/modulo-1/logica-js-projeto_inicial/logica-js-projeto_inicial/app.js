38ae3e35fa5b52ecfa2c2d7c4c78160f53ccce9b
Ryan <r.sfariaa89@gmail.com>
2025-04-24 20:54:27 +0900

    feat:finalizado a lógica do desafio.

--- modulos/modulo-1/logica-js-projeto_inicial/logica-js-projeto_inicial/app.js
+++ modulos/modulo-1/logica-js-projeto_inicial/logica-js-projeto_inicial/app.js
@@ -1,8 +1,10 @@
+
+alert("Bem vindo(a) ao desafio 4");
 //Crie um programa que utilize o console.log para exibir uma mensagem de boas-vindas.
 console.log("Bem vindo(a) ao Jogo");
 
 //Crie uma variável chamada "nome" e atribua a ela o seu nome. Em seguida, utilize o console.log para exibir a mensagem "Olá, [seu nome]!" no console do navegador.
-let nome = Andrew ;
+let nome = "Andrew";
 console.log(`Olá, ${nome}`);
 //Crie uma variável chamada "nome" e atribua a ela o seu nome. Em seguida, utilize o alert para exibir a mensagem "Olá, [seu nome]!" .
 alert(`Olá, ${nome}`);
@@ -24,16 +26,46 @@ let subtracao = Valor1 - Valor2;
 let resultadoSubtracao = ` A difirenca entre ${Valor1} e ${Valor2} é igual a  ${subtracao}`;
 alert(resultadoSubtracao);
 //Peça ao usuário para inserir sua idade com prompt. Com base na idade inserida, utilize um if para verificar se a pessoa é maior ou menor de idade, exibindo uma mensagem apropriada no console.
-
+let idade =prompt("Qual a sua idade")
+if(idade >=18){
+     alert("Você pode tirar carteira de qualquer veiculo")
+}else{
+    alert("Você ainda não tem idade o suficiente para tirar a carteira de veiculos")
+}
 //Crie uma variável "numero" e peça um valor com prompt verifique se é positivo, negativo ou zero. Use if-else para imprimir a respectiva mensagem.
+let numero = prompt("Digite um número");
+if(numero > 0){
+    alert("O número é positivo")
 
+}else if (numero <0){
+    alert("O número é negativo")
+}else{
+    alert("O número é zero")
+}
 //Use um loop while para imprimir os números de 1 a 10 no console.
-
+let numeros = 1;
+while(numeros >= 10);{
+    console.log(numeros);
+    numeros++;
+}
 //Crie uma variável "nota" e atribua um valor numérico a ela. Use if-else para determinar se a nota é maior ou igual a 7 e exiba "Aprovado" ou "Reprovado" no console.
-
+let nota = 10; // feito uma atribuição nesse caso irá sempre mostrar aprovado com o valor atual
+if(nota >= 7){
+    alert( `Aprovado,sua nota foi ${nota}` )
+}else{
+    alert("Reprovado")
+}if(nota < 7){
+    alert("Precisa fazer uma prova de recuperação para tentar atingir média 7")
+}
 //Use o Math.random para gerar qualquer número aleatório e exiba esse número no console.
-
+alert("Número aleatório")
+let numAleatorio = Math.random();
+alert(numAleatorio);
 //Use o Math.random para gerar um número inteiro entre 1 e 10 e exiba esse número no console.
-
+alert("Número aleatório entre 1 e 10")
+let numeroAleatorio = parseInt(Math.random()*10 + 1);
+alert(numeroAleatorio)
 //Use o Math.random para gerar um número inteiro entre 1 e 1000 e exiba esse número no console.
-
+alert("Número aleatório entre 1 e 1000")
+let numeroAleatorio2 = parseInt(Math.random()*1000 + 1);
+alert(numeroAleatorio2)
